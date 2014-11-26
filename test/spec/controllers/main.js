4e23453df16ab7a0f32fc7af8b99fb8ba2ffe224
Dmitry Egorov <degorov86@gmail.com>
2014-11-26 05:06:43 +0300

    - reformatted tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,27 +1,29 @@
 'use strict';
 
-describe('Controller: MainCtrl', function ()
-{
+describe('Controller: MainCtrl', function () {
     beforeEach(module('storiaApp'));
 
     var MainCtrl, scope;
     var events = 1;
 
-    beforeEach(inject(function ($controller, $rootScope)
-    {
+    beforeEach(inject(function ($controller, $rootScope) {
         scope = $rootScope.$new();
         MainCtrl = $controller('MainCtrl',
-        {
-            $scope: scope,
-            eventsProvider:
             {
-                getHomeEventsPromise: function() { return { then: function(callback){ callback(events); } }; }
-            }
-        });
+                $scope: scope,
+                eventsProvider: {
+                    getHomeEventsPromise: function () {
+                        return {
+                            then: function (callback) {
+                                callback(events);
+                            }
+                        };
+                    }
+                }
+            });
     }));
 
-    it('should attach events to the scope', function ()
-    {
+    it('should attach events to the scope', function () {
         scope.moreEvents();
         expect(scope.events).toBe(events);
     });
